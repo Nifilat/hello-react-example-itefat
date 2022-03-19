73793010ef013a4f4965dd8f10c7e571b43e08e5
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-19 03:07:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello-react-example-itefat
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hello-react-example-itefat)
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+function Hello() {
+  return <h1>Hello world.</h1>;
+}
